7b4f0886791c810aefe6f7a85fb7258a6950d619
Dhruv Manilawala <dhruvmanila@gmail.com>
2025-04-16 15:50:36 -0500

    use the definition scope

--- crates/red_knot_python_semantic/src/types.rs
+++ crates/red_knot_python_semantic/src/types.rs
@@ -5800,9 +5800,6 @@ pub struct FunctionType<'db> {
     /// Is this a function that we special-case somehow? If so, which one?
     known: Option<KnownFunction>,
 
-    /// The scope in which the function is defined.
-    scope: ScopeId<'db>,
-
     /// The scope that's created by the function, in which the function body is evaluated.
     body_scope: ScopeId<'db>,
 
@@ -5875,7 +5872,7 @@ impl<'db> FunctionType<'db> {
 
         // The semantic model records a use for each function on the name node. This is used here
         // to get the previous function definition with the same name.
-        let scope = self.scope(db);
+        let scope = self.definition(db).scope(db);
         let use_def = semantic_index(db, scope.file(db)).use_def_map(scope.file_scope_id(db));
         let use_id = self
             .body_scope(db)
@@ -5943,7 +5940,6 @@ impl<'db> FunctionType<'db> {
             db,
             self.name(db).clone(),
             self.known(db),
-            self.scope(db),
             self.body_scope(db),
             self.decorators(db),
             Some(generic_context),
@@ -5960,7 +5956,6 @@ impl<'db> FunctionType<'db> {
             db,
             self.name(db).clone(),
             self.known(db),
-            self.scope(db),
             self.body_scope(db),
             self.decorators(db),
             self.generic_context(db),
